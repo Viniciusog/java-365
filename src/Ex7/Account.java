fa0bbfc320c8ee85ce3d8e18d591eeec92044043
viniciusog <vinijipa23@gmail.com>
2021-12-26 15:23:49 -0400

    ex7 - final

---
+++ src/Ex7/Account.java
@@ -0,0 +1,48 @@
+package Ex7;
+
+public class Account {
+
+    private Integer number;
+    private String holder;
+    protected double balance; // total na conta
+
+    public Account() {
+
+    }
+
+    public Account(Integer number, String holder, Double balance) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return this.balance;
+    }
+
+    // saque contém uma taxa de 5 reais
+    public void withdraw(double amount) {
+        this.balance -= amount + 5;
+    }
+
+    // deposito
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+}
