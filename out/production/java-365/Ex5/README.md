c3e35f529cc9995932e084bd639cd5fd6aa58744
viniciusog <vinijipa23@gmail.com>
2021-12-26 15:10:52 -0400

    ex6

--- out/production/java-365/Ex5/README.md
+++ out/production/java-365/Ex5/README.md
@@ -1,1 +1,1 @@
-![uml-image](https://github.com/Viniciusog/java-365/blob/main/src/Ex4/img4.png)
+![uml-image](https://github.com/Viniciusog/java-365/blob/main/src/Ex5/img5.png)
---
+++ src/Ex6/SavingsAccount.java
@@ -0,0 +1,35 @@
+package Ex6;
+
+// conta poupança
+public class SavingsAccount extends Account {
+
+    private Double interestRate;
+
+    public SavingsAccount() {
+        super();
+    }
+
+    public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+        super(number, holder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void updateBalance() {
+        this.balance += this.balance * this.interestRate;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        // apenas o saque da conta poupança não contém a taxa de 5 reais
+        this.balance -= amount;
+        // super.withdraw(amount);
+    }
+}
